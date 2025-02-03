5f59feb6dfc4d3a88fb51569f2d73a19dbf99211
ailci <ali.ilci@gmx.de>
2025-02-03 12:16:18 +0100

    Browser Middleware

---
+++ Api/Middleware/BrowserAllowedMiddleware.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Api.Middleware
+{
+    public enum BrowserType
+    {
+        InternetExplorer,
+        Firefox,
+        Chrome,
+        Edge,
+        Opera,
+        SomethingElse
+    }
+
+
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class BrowserAllowedMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BrowserAllowedMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            var clientBrowserType = IdentitfyBrowserType(httpContext);
+            return _next(httpContext);
+        }
+
+        private BrowserType IdentitfyBrowserType(HttpContext httpContext)
+        {
+            var userAgent = httpContext.Request.Headers["User-Agent"][0]?.ToLower();
+            BrowserType browserType;
+
+            if (userAgent.Contains("chrome") &&
+                !(userAgent.Contains("edge") || userAgent.Contains("edg") || userAgent.Contains("opr")))
+            {
+                browserType = BrowserType.Chrome;
+            }
+            else if (userAgent.Contains("firefox"))
+            {
+                browserType = BrowserType.Firefox;
+            }
+            else if (userAgent.Contains("trident"))
+            {
+                browserType = BrowserType.InternetExplorer;
+            }
+            else if (userAgent.Contains("edge") || userAgent.Contains("edg"))
+            {
+                browserType = BrowserType.Edge;
+            }
+            else if (userAgent.Contains("opr"))
+            {
+                browserType = BrowserType.Opera;
+            }
+            else
+            {
+                browserType = BrowserType.SomethingElse;
+            }
+
+            return browserType;
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class BrowserAllowedMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseBrowserAllowedMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BrowserAllowedMiddleware>();
+        }
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -2,13 +2,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers();  //WebApi
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. ###########################################################
+//app.Use(async (context, next) =>
+//{
+//    var userAgent = context.Request.Headers["User-Agent"][0]?.ToLower();
+//    await context.Response.WriteAsync($"First middleware {userAgent}\n");
+//    await next();
+//    await context.Response.WriteAsync("First middleware Back\n");
+
+//});
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Second middleware\n");
+//    await next();
+//});
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("End middleware\n");
+//});
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
