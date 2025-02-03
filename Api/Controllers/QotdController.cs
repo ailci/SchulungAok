dcbffb74a4af0ba7b3113af03273e8f9094e6adc
ailci <ali.ilci@gmx.de>
2025-02-03 14:56:57 +0100

    Dto

---
+++ Api/Controllers/QotdController.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DataTransferObjects;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]  // localhost:1234/api/qotd
+[ApiController]
+public class QotdController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetQuoteOfTheDay()  //localhost:1234/api/qotd
+    {
+        var qotd = new QuoteOfTheDayDto
+        {
+            Id = Guid.NewGuid(),
+            QuoteText = "Larum lierum Löffelstiel, wer nicht fragt, der weiß nicht viel!",
+            AuthorName = "Ich",
+            AuthorDescription = "Dozent",
+            AuthorBirthDate = DateOnly.FromDateTime(DateTime.Today)
+        };
+
+        return Ok(qotd);
+    }
+}
---
+++ Domain/Entities/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Quote
+    {
+        public Guid Id { get; set; } // Id,Id, Klassenname + Id
+        public required string QuoteText { get; set; }
+        public Author? Author { get; set; }
+        public Guid AuthorId { get; set; } // FK => Author + Id
+    }
+}
