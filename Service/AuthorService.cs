04273e5d856cd9555290201a4b4df336cf351d01
ailci <ali.ilci@gmx.de>
2025-02-04 15:20:23 +0100

    Mapping Author

--- Service/AuthorService.cs
+++ Service/AuthorService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Persistence.Contracts;
 using Shared.DataTransferObjects;
 
@@ -11,24 +12,28 @@ namespace Service;
 public class AuthorService : IAuthorService
 {
     private readonly IRepositoryManager _repositoryManager;
+    private readonly IMapper _mapper;
 
-    public AuthorService(IRepositoryManager repositoryManager)
+    public AuthorService(IRepositoryManager repositoryManager, IMapper mapper)
     {
         _repositoryManager = repositoryManager;
+        _mapper = mapper;
     }
 
     public async Task<IEnumerable<AuthorDto>> GetAuthorsAsync()
     {
         var authors = await _repositoryManager.Author.GetAuthorsAsync();
 
-        return authors.Select(author => new AuthorDto
-        {
-            Id = author.Id,
-            Name = author.Name,
-            Description = author.Description,
-            BirthDate = author.BirthDate,
-            Photo = author.Photo,
-            PhotoMimeType = author.PhotoMimeType
-        });
+        //return authors.Select(author => new AuthorDto
+        //{
+        //    Id = author.Id,
+        //    Name = author.Name,
+        //    Description = author.Description,
+        //    BirthDate = author.BirthDate,
+        //    Photo = author.Photo,
+        //    PhotoMimeType = author.PhotoMimeType
+        //});
+
+        return _mapper.Map<IEnumerable<AuthorDto>>(authors);
     }
 }
