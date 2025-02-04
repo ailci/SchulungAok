4a5b2154f9c09f71cef08e362eb4d686509026ca
ailci <ali.ilci@gmx.de>
2025-02-04 10:12:27 +0100

    Qotd

--- Api/Controllers/QotdController.cs
+++ Api/Controllers/QotdController.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 using Shared.DataTransferObjects;
 
@@ -19,9 +20,23 @@ public class QotdController : ControllerBase
 
     
     [HttpGet]
-    public IActionResult GetQuoteOfTheDay()  //localhost:1234/api/qotd
+    public async Task<IActionResult> GetQuoteOfTheDayAsync()  //localhost:1234/api/qotd
     {
-        var quotes = _context.Quotes.ToList();
-        return Ok(quotes);
+        var quotes = await _context.Quotes.Include(c => c.Author).ToListAsync();
+        var random = new Random();
+        var randomQuote = quotes[random.Next(quotes.Count)];
+
+        var qotd = new QuoteOfTheDayDto
+        {
+            Id = randomQuote.Id,
+            QuoteText = randomQuote.QuoteText,
+            AuthorName = randomQuote.Author?.Name ?? "",
+            AuthorDescription = randomQuote.Author?.Description ?? "",
+            AuthorBirthDate = randomQuote.Author?.BirthDate,
+            AuthorPhoto = randomQuote.Author?.Photo,
+            AuthorPhotoMimeType = randomQuote.Author?.PhotoMimeType
+        };
+
+        return Ok(qotd);
     }
 }
