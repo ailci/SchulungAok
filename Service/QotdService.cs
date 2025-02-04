6ee2dca5bded553b20deb900c3d10d70493122a9
ailci <ali.ilci@gmx.de>
2025-02-04 15:01:41 +0100

    Automapper

--- Service/QotdService.cs
+++ Service/QotdService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Persistence.Contracts;
 using Shared.DataTransferObjects;
 
@@ -11,25 +12,31 @@ namespace Service;
 public class QotdService : IQotdService
 {
     private readonly IRepositoryManager _repositoryManager;
+    private readonly IMapper _mapper;
 
-    public QotdService(IRepositoryManager repositoryManager)
+    public QotdService(IRepositoryManager repositoryManager, IMapper mapper)
     {
         _repositoryManager = repositoryManager;
+        _mapper = mapper;
     }
 
     public async Task<QuoteOfTheDayDto> GetQuoteOfTheDayAsync()
     {
         var randomQuote = await _repositoryManager.Quote.GetRandomQuoteAsync();
 
-        return new QuoteOfTheDayDto
-        {
-            Id = randomQuote.Id,
-            QuoteText = randomQuote.QuoteText,
-            AuthorName = randomQuote.Author?.Name ?? "",
-            AuthorDescription = randomQuote.Author?.Description ?? "",
-            AuthorBirthDate = randomQuote.Author?.BirthDate,
-            AuthorPhoto = randomQuote.Author?.Photo,
-            AuthorPhotoMimeType = randomQuote.Author?.PhotoMimeType
-        };
+        // 1. Version Klassik
+        //return new QuoteOfTheDayDto
+        //{
+        //    Id = randomQuote.Id,
+        //    QuoteText = randomQuote.QuoteText,
+        //    AuthorName = randomQuote.Author?.Name ?? "",
+        //    AuthorDescription = randomQuote.Author?.Description ?? "",
+        //    AuthorBirthDate = randomQuote.Author?.BirthDate,
+        //    AuthorPhoto = randomQuote.Author?.Photo,
+        //    AuthorPhotoMimeType = randomQuote.Author?.PhotoMimeType
+        //};
+
+        // 2.Version Automapper
+        return _mapper.Map<QuoteOfTheDayDto>(randomQuote);
     }
 }
