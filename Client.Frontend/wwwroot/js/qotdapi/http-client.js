acfdcd3c0c6b8560b38fa1034cb1af49fa72bf26
ailci <ali.ilci@gmx.de>
2025-02-06 15:26:02 +0100

    Final

--- Client.Frontend/wwwroot/js/qotdapi/http-client.js
+++ Client.Frontend/wwwroot/js/qotdapi/http-client.js
@@ -15,4 +15,21 @@ export class HttpClient {
 
         return response.json();
     }
+
+    async getWithApiKey(url, apikey) {
+        let response;
+
+        try {
+            response = await fetch(url, {
+                headers: { "x-api-key": `${apikey}` }
+            });
+            console.log(response);
+
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+
+        return response.json();
+    }
 }
