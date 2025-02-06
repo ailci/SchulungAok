c10765147beddd381c96a4061a5eeb8c7f78fc3f
ailci <ali.ilci@gmx.de>
2025-02-06 14:36:05 +0100

    qotdui

--- Client.Frontend/wwwroot/js/qotdapi/main.js
+++ Client.Frontend/wwwroot/js/qotdapi/main.js
@@ -1,1 +1,9 @@
 ﻿
+
+const qotdUI = {
+    quoteTextEl: document.querySelector("#quote"),
+    authorPhotoEl: document.querySelector("#author-photo"),
+    authorNameEl: document.querySelector("#author-name"),
+    authorDescriptionEl: document.querySelector("#author-description"),
+    authorBirthdateEl: document.querySelector("#author-birthdate")
+};
