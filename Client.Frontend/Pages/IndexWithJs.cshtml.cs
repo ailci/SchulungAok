5bed9e6aa4b5570123c044dab36e5186554376bc
ailci <ali.ilci@gmx.de>
2025-02-06 14:25:04 +0100

    indewithjs

---
+++ Client.Frontend/Pages/IndexWithJs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Client.Frontend.Pages
+{
+    public class IndexWithJsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
