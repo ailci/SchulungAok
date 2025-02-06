cf984d65d6d8643ccda177314ccfb65736f8c424
ailci <ali.ilci@gmx.de>
2025-02-06 12:26:01 +0100

    Typed Client

--- Client.Frontend/Services/QotdApiService.cs
+++ Client.Frontend/Services/QotdApiService.cs
@@ -3,15 +3,13 @@ using System.Net.Http;
 
 namespace Client.Frontend.Services;
 
-public class QotdApiService(ILogger<QotdApiService> logger, IHttpClientFactory httpClientFactory) : IQotdApiService
+public class QotdApiService(ILogger<QotdApiService> logger, HttpClient client) : IQotdApiService
 {
     private const string QotdUri = "qotd";
 
     public async Task<QuoteOfTheDayDto> GetQuoteOfTheDayAsync()
     {
         logger.LogInformation($"{nameof(GetQuoteOfTheDayAsync)} aufgerufen...");
-
-        var client = httpClientFactory.CreateClient("qotdapiservice");
 
         var response = await client.GetFromJsonAsync<QuoteOfTheDayDto>(QotdUri);
         if (response is null) throw new HttpRequestException("Konnte Qotd nicht holen");
