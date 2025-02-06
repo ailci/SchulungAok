70bb362fe301aa2b9ffb6cc0951210e5ae859acd
ailci <ali.ilci@gmx.de>
2025-02-06 12:15:07 +0100

    Service

--- Client.Frontend/Pages/Index.cshtml.cs
+++ Client.Frontend/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Shared.DataTransferObjects;
 using System.Text.Json;
+using Client.Frontend.Services;
 
 namespace Client.Frontend.Pages;
 
@@ -9,26 +10,37 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IQotdApiService _apiService;
     public QuoteOfTheDayDto? QotdDto { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
+    public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory, IQotdApiService apiService)
     {
         _logger = logger;
         _httpClientFactory = httpClientFactory;
+        _apiService = apiService;
     }
 
     public async Task OnGet()
     {
-        // 1. Version
-        //var client = _httpClientFactory.CreateClient("qotdapiservice");
-        //var response = await client.GetAsync("qotd"); // BaseUri + Individuelle => localhost:1234/api/qotd
-        //response.EnsureSuccessStatusCode();
-        //var content = await response.Content.ReadAsStringAsync();
-        //QotdDto = JsonSerializer.Deserialize<QuoteOfTheDayDto>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+        try
+        {
+            // 1. Version
+            //var client = _httpClientFactory.CreateClient("qotdapiservice");
+            //var response = await client.GetAsync("qotd"); // BaseUri + Individuelle => localhost:1234/api/qotd
+            //response.EnsureSuccessStatusCode();
+            //var content = await response.Content.ReadAsStringAsync();
+            //QotdDto = JsonSerializer.Deserialize<QuoteOfTheDayDto>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
 
-        // 2. Version Abkürzung
-        var client = _httpClientFactory.CreateClient("qotdapiservice");
-        QotdDto = await client.GetFromJsonAsync<QuoteOfTheDayDto>("qotd");
+            // 2. Version Abkürzung
+            //var client = _httpClientFactory.CreateClient("qotdapiservice");
+            //QotdDto = await client.GetFromJsonAsync<QuoteOfTheDayDto>("qotd");
 
+            // 3. Version via Service
+            QotdDto = await _apiService.GetQuoteOfTheDayAsync();
+        }
+        catch (HttpRequestException e)
+        {
+           _logger.LogError($"{e.StatusCode} ### {e.Message}");
+        }
     }
 }
---
+++ Client.Frontend/Services/QotdApiService.cs
@@ -0,0 +1,21 @@
+﻿using Shared.DataTransferObjects;
+using System.Net.Http;
+
+namespace Client.Frontend.Services;
+
+public class QotdApiService(ILogger<QotdApiService> logger, IHttpClientFactory httpClientFactory) : IQotdApiService
+{
+    private const string QotdUri = "qotd";
+
+    public async Task<QuoteOfTheDayDto> GetQuoteOfTheDayAsync()
+    {
+        logger.LogInformation($"{nameof(GetQuoteOfTheDayAsync)} aufgerufen...");
+
+        var client = httpClientFactory.CreateClient("qotdapiservice");
+
+        var response = await client.GetFromJsonAsync<QuoteOfTheDayDto>(QotdUri);
+        if (response is null) throw new HttpRequestException("Konnte Qotd nicht holen");
+
+        return response;
+    }
+}
